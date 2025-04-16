12634f43606a06f7cb7996cf0348830591a3a085
Thomas Kragerud <thomas.kragerud@gmail.com>
2025-04-16 00:08:52 -0700

    added docs

---
+++ docs/astro/tgaTextureLoader.Cu0-CMeC.js
@@ -0,0 +1,1 @@
+import{g as r,h as n}from"./RobotViewer.BWtS4x29.js";import"./index.9MVAkNgL.js";class d{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,a){const o=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),s=r(o);a(s.width,s.height,t.generateMipMaps,!1,()=>{n(t,o)})}}export{d as _TGATextureLoader};
