83f32296f72b21fbbfb43f062dab05fa3d66a983
Thomas Kragerud <thomas.kragerud@gmail.com>
2025-04-15 19:46:36 -0700

    new build

---
+++ docs/astro/BabylonViewer.CCaydGVe.js
@@ -0,0 +1,1 @@
+import{y as a}from"./BabylonViewer.CHmMaoQ8.js";import"./index.BVOCwoKb.js";export{a as default};
--- docs/astro/BabylonViewer.Dfpkb2V6.js
+++
@@ -1,1 +0,0 @@
-import{y as a}from"./BabylonViewer.BR4eqIE6.js";import"./index.BVOCwoKb.js";export{a as default};
