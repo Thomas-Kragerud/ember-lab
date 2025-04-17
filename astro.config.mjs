4f040dff2d3dc195e327fd0cb833bbd0a6495a4c
Thomas Kragerud <thomas.kragerud@gmail.com>
2025-04-17 14:26:11 -0700

    removed docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Thomas-Kragerud.github.io', // Replace with your GitHub username
-  base: '/ember-lab',                       // Replace with your repository name
+ // base: '/ember-lab',                       // Replace with your repository name
   output: "static",
   integrations: [react()],
   outDir: "./docs",
@@ -17,7 +17,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   build: {
-    assets: 'astro', // This ensures asset names don't have underscores
+    assets: 'astro',                 // This ensures asset names don't have underscores
     assetsPrefix: "./"
   }
 });
